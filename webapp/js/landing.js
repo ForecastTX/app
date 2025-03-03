0ee393a58845194cc28f5e3955c4720298734dc3
ForecastTX <kxs7244@mavs.uta.edu>
2025-03-03 13:30:53 -0600

    Update landing.js

--- webapp/js/landing.js
+++ webapp/js/landing.js
@@ -1,6 +1,6 @@
  window.onload = function() {
             document.getElementById("welcomeText").style.opacity = 1;
             setTimeout(function() {
-                window.location.href = '../html/login.html';
+                window.location.href = '/login.html';
             }, 3000); // Redirect after 3 seconds
         };
