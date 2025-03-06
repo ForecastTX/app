d56710e10795749e9669c56dfc9e67d7437b8f59
ForecastTX <kxs7244@mavs.uta.edu>
2025-03-06 13:07:04 -0600

    Update login.js

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const createAccountBtn = document.getElementById("createAccountBtn");
+
+    if (createAccountBtn) {
+        createAccountBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+            window.location.href = "createAccount.html";
+        });
+    }
+});
 
